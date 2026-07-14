41873a1e18c57e1d65014617d355f9100955f3ea
Per Lundgren <lundgrenper.pl@gmail.com>
2026-07-14 12:31:56 +0200

    Google web-klient-id för Drive-inloggningen

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 // OAuth-klient av typen "Webbapplikation" – används av Google Sign-In på
 // Android för att få id-/accesstoken. Ersätt platshållaren när klienten
 // är skapad; appen visar "inte konfigurerad" tills dess.
-export const GOOGLE_WEB_CLIENT_ID = "REPLACE_WITH_WEB_CLIENT_ID.apps.googleusercontent.com";
+export const GOOGLE_WEB_CLIENT_ID =
+  "908746566063-2g4ohvdtb4ed4flk0fnb1rvct9ag64st.apps.googleusercontent.com";
 
 export const isGoogleConfigured = () => !GOOGLE_WEB_CLIENT_ID.startsWith("REPLACE");
